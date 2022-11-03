7ef4bb697c498e0ebde183de774f73127f7baebb
inayatullahaaxioned <inayatullaha@axioned.com>
2022-11-03 15:31:40 +0530

    edit and delete btn js added

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,9 @@ var form = document.querySelector('#form'),
   lastname = document.querySelector('#lastname'),
   gender = form.gender,
   address = document.querySelector('#address'),
-  checkbox = document.querySelector("#terms");
+  checkbox = document.querySelector("#terms"),
+  isEdit = false,
+  editRow;
 
 //Grabbing buttons 
 var submitBtn = document.querySelector('.submit-btn'),
@@ -34,22 +36,13 @@ function validateForm(e) {
       address: address.value,
       checkbox: checkbox.checked,
     }
-    display();
-    alert('form is submitted');
-    function display() {
-      var list = document.createElement('li');
-      list.className = "row";
-      var table = document.querySelector('.table');
-      list.innerHTML = `<ul class="cols">
-              <li class="col">${userData.fname}</li>
-              <li class="col">${userData.lname}</li>
-              <li class="col">${userData.gender}</li>
-              <li class="col">${userData.address}</li>
-              <li class="col"><button class="edit-btn">Edit</button></li>
-              <li class="col"><button class="delete-btn">Delete</button></li>
-            </ul>`;
-      table.appendChild(list);
+    if (isEdit) {
+      EditFunc();
+    } else {
+      display(userData);
     }
+    alert('form is submitted');
+
 
     firstname.value = "";
     lastname.value = "";
@@ -121,6 +114,38 @@ function validateCheckbox(input) {
   }
 }
 
+//display function for showing data in table
+function display(dataList) {
+  var table = document.querySelector('.table');
+  var list = document.createElement('li'),
+    ul = document.createElement('ul');
+  ul.classList.add('cols');
+  list.classList.add('row');
+  var data = [dataList.fname, dataList.lname, dataList.gender, dataList.address];
+  for (var i = 0; i < 6; i++) {
+    var li = document.createElement('li');
+    li.classList.add('col');
+    if (i < 4) {
+      li.innerText = data[i];
+    } else {
+      var Btn = document.createElement('button');
+      if (i == 4) {
+        Btn.classList.add('edit-btn');
+        Btn.innerText = "Edit";
+        Btn.addEventListener('click', EditData);
+      } else {
+        Btn.classList.add('delete-btn');
+        Btn.innerText = "Delete";
+        Btn.addEventListener('click', DeleteData);
+      }
+      li.appendChild(Btn);
+    }
+    ul.appendChild(li);
+  }
+  list.appendChild(ul);
+  table.appendChild(list);
+}
+
 //cancel btn form reset
 cancelBtn.addEventListener('click', formReset);
 function formReset() {
@@ -129,4 +154,35 @@ function formReset() {
   error.forEach(function remove(elem) {
     elem.remove();
   })
+}
+
+function EditData() {
+  isEdit = true;
+  var parent = this.parentElement.parentElement;
+  editRow = parent;
+  firstname.value = parent.children[0].innerText;
+  lastname.value = parent.children[1].innerText;
+  address.value = parent.children[3].innerText;
+  checkbox.checked = true;
+  gender.forEach(function (input) {
+    if (input.value == parent.children[2].innerText) {
+      input.checked = true;
+    }
+  })
+}
+
+function DeleteData() {
+  this.parentElement.parentElement.remove();
+}
+
+function EditFunc() {
+  editRow.children[0].innerText = firstname.value;
+  editRow.children[1].innerText = lastname.value;
+  editRow.children[3].innerText = address.value;
+  gender.forEach(function (input) {
+    if (input.checked) {
+      editRow.children[2].innerText = input.value;
+    }
+  })
+  isEdit = false;
 }
